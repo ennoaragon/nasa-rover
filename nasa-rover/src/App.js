d8b3592410d472d011459b50e09fadb471f3f5e3
ennoaragon <enno.aragon@oit.edu>
2021-08-17 07:44:11 -0700

    working api call

--- nasa-rover/src/App.js
+++ nasa-rover/src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useContext } from 'react';
 import {Route, Switch} from 'react-router-dom';
-
+import axios from 'axios'
 // pages
 import Home from './pages/Home';
 import Curiosity from './pages/Curiosity';
@@ -10,15 +10,21 @@ import Spirit from './pages/Spirit';
 // styles
 import './App.css';
 import Navbar from './components/Navbar';
+
+
+
+axios.defaults.baseURL = process.env.REACT_APP_AXIOS_NASA_ROVER;
+axios.defaults.headers.common['Authorization'] = process.env.REACT_APP_AXIOS_API_KEY;
+
 function App() {
 
   // Begin with the fetch call for rover data
-  useEffect(() => {
+  // useEffect(() => {
     
-    return () => {
+  //   return () => {
       
-    }
-  }, [])
+  //   }
+  // }, [])
 
   return (
     <div className="App">
--- nasa-rover/src/pages/Curiosity.js
+++ nasa-rover/src/pages/Curiosity.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 
 const Curiosity = () => {
+  
+  const [content, setContent] = useState(null);
+
+  //TODO: Create helper functions to parse through the images
+
+  // https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=3&api_key=fr9whaDN6x918bcnGALL8UWnf1fkHYqItqxxz7dw
+  // get curiosity images from nasa api
+  useEffect(() => {
+    const apiCall = async () => {
+      // axios.get(`/curiosity/photos`,null, { params: {
+      //   sol: "sol=0",
+      //   api_key: process.env.REACT_APP_AXIOS_API_KEY,
+      // }})
+      axios.get("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=3&api_key=fr9whaDN6x918bcnGALL8UWnf1fkHYqItqxxz7dw")
+      .then(res=> console.log("Data: ", res.data)).catch(err => console.log("error: ", err.message))
+    }
+    apiCall();
+
+  }, [])
+
+
   return (
     <div>
       <h1>Curiosity!</h1>
+      <p>{content}</p>
     </div>
   )
 }
